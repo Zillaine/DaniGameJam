8acf244f939db1f525dc05b67b0021cc5eb33a76
Marco Túlio <marcotmotta@gmail.com>
2021-01-02 13:35:07 -0300

    shooting cookies

--- scripts/chapeuzinho.gd
+++ scripts/chapeuzinho.gd
@@ -1,10 +1,14 @@
 extends "enemy.gd"
 
+var cookie_scene = preload("res://scenes/Cookie.tscn")
+
 var timer = 2
 
 func _ready():
 	$AttackTimer.start(timer)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_AttackTimer_timeout():
+	var cookie = cookie_scene.instance()
+	cookie.position = position
+	cookie.direction = (get_parent().get_node('Player').position - cookie.position).normalized()
+	get_parent().add_child(cookie)
